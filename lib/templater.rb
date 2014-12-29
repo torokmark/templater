95d80148a8c2386d805ae6ff95ca56924f1d863f
Mark Torok <torok.marko@gmail.com>
2014-12-29 15:20:07 +0100

    Add documentation to replace method

--- lib/templater.rb
+++ lib/templater.rb
@@ -1,4 +1,11 @@
 module Templater
+  # Replaces templates in a string with the values.
+  #
+  # @note templates in a string are supposed to be like `{{temp}}`
+  #
+  # @param str [String] the string with templates.
+  # @param values [Hash, Array] the values to replace templates
+  # @return [String] the string with replaced values.
   def self.replace(str, values)
     if values.is_a? Hash
         str.gsub(/{{(.*?)}}/) { 
